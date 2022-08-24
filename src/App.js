ede2e57d4878743a7b2224c7c1ee64fc67b19ac2
Micaela <micaela.alvarez@nimacloud.com>
2022-08-24 15:18:46 -0300

    update porfolio

--- src/App.js
+++ src/App.js
@@ -1,88 +1,41 @@
 import React from "react";
-import "./App.css";
-import Foto from "./Media/Mask Group.png";
-import Technologies from "./Components/Technologies";
-import Carrousel from "./Components/Carrousel";
-import Form from "./Components/Form";
-import Links from "./Components/Links";
-import AOS from "aos";
-import "aos/dist/aos.css";
-AOS.init();
 
 function App() {
-  return (
-    <div className="container">
-      <div className="header">
-        <div className="item1">
-          <h1 className="title" data-aos="fade-down" data-aos-duration="3000">
-            MICAELA ALESSANDRA ALVAREZ
-          </h1>
-          <h3
-            className="subtitle"
-            data-aos="fade-right"
-            data-aos-duration="3000"
-          >
-            FULL STACK DEVELOPER
-          </h3>
-          <Links />
-          <img src={Foto} alt="" className="foto" />
-        </div>
-        <div className="aboutMe" data-aos="fade-right" data-aos-duration="3000">
-          <p>ABOUT ME</p>
-        </div>
-        <div className="item2"></div>
-      </div>
-      <div className="textZone">
-        <p className="text1">
-          “I’M A FULL STACK DEVELOPER, WHO LOVES TO SOLVE CHALLENGES, TO FUSE,
-          <br />
-          CREATIVITY, INNOVATION, STRATEGY AND CRAFT. I LOVE TO LEARN SO I’M
-          CURRENTLY
-          <br />
-          IMPROVING MY EXPERIENCE ON TYPESCRIPT, GRAPHQL, APOLLO AND MONGODB.
-          <br />
-          I HAVE ONE GOAL TO REACH, THAT IS TO DEVELOP AS A PROFESSIONAL <br />
-          IN THE TECHNOLOGY WORLD, THAT IS WHY I’M LOOKING FOR AN <br />
-          OPPORTUNITY TO TAKE MY EXPERIENCE <br />
-          TO THE NEXT LEVEL”
-        </p>
-      </div>
-      <div className="technologies">
-        <Technologies />
-      </div>
-      <div className="skills" data-aos="fade-left" data-aos-duration="3000">
-        <p>TECHNOLOGIES</p>
-      </div>
-      <div className="item3"></div>
-      <div className="textZone1">
-        <p className="text2">
-          “I CONSIDER MYSELF A VERSATILE AND <br /> COLLABORATIVE PERSON, ALWAYS
-          WILLING TO MOTIVATE THE TEAM,
-          <br /> WITH GREAT CAPACITY TO COMMUNICATE AND ADAPT TO ANY
-          ENVIRONMENT”
-        </p>
-      </div>
-      <div className="item4"></div>
-      <div className="projects" data-aos="fade-right" data-aos-duration="3000">
-        <p>PROJECTS</p>
-      </div>
-      <div className="item6">
-        <Carrousel />
-      </div>
-      <div className="item5"></div>
-      <div className="contactMe" data-aos="fade-left" data-aos-duration="3000">
-        <p>CONTACT ME</p>
-      </div>
-      <div
-        id="contactSection"
-        className="item7"
-        data-aos="zoom-out-up"
-        data-aos-duration="2000"
-      >
-        <Form />
-      </div>
-    </div>
-  );
+	return (
+		<div className="2xl:container 2xl:mx-0 h-screen bg-general-background py-8">
+			<div className="bg-yellow-background h-32 w-3/5 ml-8">
+				<h1>MICAELA ALESSANDRA ALVAREZ</h1>
+				<h3>Full Stack Developer</h3>
+			</div>
+			<div className="bg-purple-background h-96 w-2/5 ml-96">
+				<p>
+					`Hey there ! thank you for spend your time and take a look
+					on my work. I’m a person who loves to solve challenges, to
+					fuse creativity, innovation, strategy and craft. I’m a very
+					curious person so I like to learn new things every time.`
+				</p>
+				<p>
+					`I’m got experience working as full stack developer in
+					Kaptain App at Nimacloud Company. Before that I learned new
+					tools as Docker and Jenkins in Tsoft Company where I worked
+					as Devops. Currently, I’m learning and working on projects
+					related to Salesforce.`
+				</p>
+			</div>
+			<div className="bg-green-background h-32 w-3/5 ml-8">
+				<p>
+					`I invite you to see my work on the links below and check my
+					repository, to see how much I learned when I studied on
+					Henry’s Bootcamp and the projects I developed for my self.
+				</p>
+				<p>
+					Every comment you have about my work will be received
+					gratefully. I think that the constructive critisism is the
+					best way to grow and improve skil`
+				</p>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				"general-background": "#3D3D3D",
+				"yellow-background": "#FFEF89",
+				"green-background": "#DDFF94",
+				"purple-background": "#D094FF",
+			},
+		},
+	},
+	plugins: [],
+};
